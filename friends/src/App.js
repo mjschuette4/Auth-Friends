b27ab944f34e42d30ecfa44517efe24e8f8c1fd6
MatthewSchuette <mjschuette4@gmail.com>
2020-04-21 17:26:38 -0500

    Setup App.js routing

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import PrivateRoute from './components/PrivateRoute';
-import FriendsList from './components/FriendsList';
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./actions/AddFriend";
+import UpdateFriend from "./actions/UpdateFriend";
+import DeleteFriend from "./actions/DeleteFriend";
+import PrivateRoute from "./components/PrivateRoute";
+
 
 class App extends React.Component {
 
@@ -16,15 +20,23 @@ render(){
   return (
     <div className="App">
       <Router>
-        <div className="Nav">
-          <Route path="/login" component={login} />
-          <PrivateRoute exact path="/protected" component={FriendsList} />
-        </div>
+        <Link to='/login'>Login</Link>
+        <Link to='/friends-list'>Friends List</Link>
+        <Link to='/update-friend'>Update Friend</Link>
+        <Link to='/add-friend'>Add Friend</Link>
+        <Link to='/delete-friend'>Delete Friend</Link>
+
+        <Switch>
+          <PrivateRoute exact path='/friends-list' component={FriendsList} />
+          <PrivateRoute exact path='/add-friend' component={AddFriend} />
+          <PrivateRoute exact path='/delete-friend' component={DeleteFriend} />
+          <PrivateRoute exact path='/update-friend' component={UpdateFriend} />
+          <Route path='/login' component={Login} />
+        </Switch>
       </Router>
     </div>
-  );
+    );
+  }
 }
-}
-
 
 export default App;
